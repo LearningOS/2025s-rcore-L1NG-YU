c6922118e9225436cd1f1583adce8eed09bdd9b8
l1ngyu <2306010340@hhu.edu.cn>
2025-04-25 10:30:37 +0800

    update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,10 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use crate::{
+        //config::MAX_SYSCALL_NUM,
+        task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next,current_user_token}, 
+        timer::get_time_us,
+        
+        };
 
 #[repr(C)]
 #[derive(Debug)]
@@ -26,8 +31,13 @@ pub fn sys_yield() -> isize {
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!("kernel: sys_get_time");
-    -1
+    let us =get_time_us();
+    let ts = translated_struct_ptr(current_user_token(), _ts);
+    *ts = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
